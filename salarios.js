26d45170580bb44db25b58b7365532bb933d5208
Carmen Valero <mvalero4@hotmail.es>
2022-06-03 16:08:41 -0500

    Fin del curso

---
+++ salarios.js
@@ -0,0 +1,41 @@
+const colombia = [];
+
+    colombia.push({
+        name: "Carmen",
+        salary: 1000,
+    });
+
+    colombia.push({
+        name: "Carlos",
+        salary: 1500,
+    });
+
+    colombia.push({
+        name: "Mauro",
+        salary: 2000,
+    });
+
+    colombia.push({
+        name: "David",
+        salary: 1800,
+    });
+
+    colombia.push({
+        name: "Luis",
+        salary: 2200,
+    });
+
+    colombia.push({
+        name: "Ana",
+        salary: 1300,
+    });
+
+    colombia.push({
+        name: "Angel",
+        salary: 1450,
+    });
+
+    colombia.push({
+        name: "Daniel",
+        salary: 2100,
+    });
